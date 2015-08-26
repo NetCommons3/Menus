0e2c054449348c92899b31cc2afc026c3c0b0b2b
s-nakajima <nakajimashouhei@gmail.com>
2015-08-26 16:14:05 +0900

    phpdocエラー

--- Config/Migration/1440041616_init.php
+++ Config/Migration/1440041616_init.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Migration file
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * Migration file
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Menus\Config\Migration
+ */
 class Init extends CakeMigration {
 
 /**
