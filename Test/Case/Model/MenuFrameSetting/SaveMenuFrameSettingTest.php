55c9bd9edf0492c0f3e082359f9ecef5fadab507
s-nakajima <nakajimashouhei@gmail.com>
2016-03-07 08:49:50 +0900

    MenuFrameSetting::saveMenuFrameSetting()のテスト

---
+++ Test/Case/Model/MenuFrameSetting/SaveMenuFrameSettingTest.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * MenuFrameSetting::saveMenuFrameSetting()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsSaveTest', 'NetCommons.TestSuite');
+App::uses('MenuFrameSettingFixture', 'Menus.Test/Fixture');
+
+/**
+ * MenuFrameSetting::saveMenuFrameSetting()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Menus\Test\Case\Model\MenuFrameSetting
+ */
+class MenuFrameSettingSaveMenuFrameSettingTest extends NetCommonsSaveTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.menus.menu_frame_setting',
+		'plugin.menus.menu_frames_page',
+		'plugin.menus.menu_frames_room',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'menus';
+
+/**
+ * Model name
+ *
+ * @var string
+ */
+	protected $_modelName = 'MenuFrameSetting';
+
+/**
+ * Method name
+ *
+ * @var string
+ */
+	protected $_methodName = 'saveMenuFrameSetting';
+
+/**
+ * POSTリクエストデータ生成
+ *
+ * @return array リクエストデータ
+ */
+	private function __data() {
+		$data = array(
+			'MenuFrameSetting' => array(
+				'id' => '3',
+				'frame_key' => 'frame_3',
+				'display_type' => 'main',
+			),
+			'Menus' => array(
+				1 => array(
+					1 => array(
+						'MenuFramesPage' => array(
+							'id' => '1',
+							'frame_key' => 'frame_3',
+							'page_id' => '1',
+							'is_hidden' => false,
+						)
+					),
+					2 => array(
+						'MenuFramesPage' => array(
+							'id' => null,
+							'frame_key' => 'frame_3',
+							'page_id' => null,
+							'is_hidden' => null,
+						)
+					)
+				),
+			),
+			'MenuRooms' => array(
+				1 => array(
+					'MenuFramesRoom' => array(
+						'id' => '1',
+						'frame_key' => 'frame_3',
+						'room_id' => '1',
+						'is_hidden' => false,
+					)
+				)
+			)
+		);
+
+		return $data;
+	}
+
+/**
+ * Save用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSave() {
+		$data = $this->__data();
+
+		$results = array();
+		// * 編集の登録処理
+		$results[0] = array($data);
+		// * 新規の登録処理
+		$results[1] = array($data);
+		$results[1] = Hash::insert($results[1], '0.MenuFrameSetting.id', null);
+		$results[1] = Hash::remove($results[1], '0.MenuFrameSetting.created_user');
+
+		return $results;
+	}
+
+/**
+ * SaveのExceptionError用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - mockModel Mockのモデル
+ *  - mockMethod Mockのメソッド
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSaveOnExceptionError() {
+		$data = $this->dataProviderSave()[0][0];
+
+		return array(
+			array($data, 'Menus.MenuFrameSetting', 'save'),
+		);
+	}
+
+/**
+ * SaveのValidationError用DataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *  - mockModel Mockのモデル
+ *  - mockMethod Mockのメソッド(省略可：デフォルト validates)
+ *
+ * @return array テストデータ
+ */
+	public function dataProviderSaveOnValidationError() {
+		$data = $this->dataProviderSave()[0][0];
+
+		return array(
+			array($data, 'Menus.MenuFrameSetting'),
+		);
+	}
+
+}
