ebbfd67b41dd09b8fea14ef19b0af475f17bbaa9
s-nakajima <nakajimashouhei@gmail.com>
2016-03-06 19:03:46 +0900

    beforeRenderに修正

--- View/Helper/MenuHelper.php
+++ View/Helper/MenuHelper.php
@@ -34,19 +34,17 @@ class MenuHelper extends AppHelper {
 	);
 
 /**
- * After render file callback.
- * Called after any view fragment is rendered.
+ * Before render callback. beforeRender is called before the view file is rendered.
  *
  * Overridden in subclasses.
  *
- * @param string $viewFile The file just be rendered.
- * @param string $content The content that was rendered.
+ * @param string $viewFile The view file that is going to be rendered
  * @return void
  */
-	public function afterRenderFile($viewFile, $content) {
-		$content = $this->NetCommonsHtml->css('/menus/css/style.css') .
-					$this->NetCommonsHtml->script('/menus/js/menus.js') . $content;
-		parent::afterRenderFile($viewFile, $content);
+	public function beforeRender($viewFile) {
+		$this->NetCommonsHtml->css('/menus/css/style.css');
+		$this->NetCommonsHtml->script('/menus/js/menus.js');
+		parent::beforeRender($viewFile);
 	}
 
 /**
