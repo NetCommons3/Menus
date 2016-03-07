b19b01568d30060cad9b0e6bded39067a1fecf79
s-nakajima <nakajimashouhei@gmail.com>
2016-03-07 14:18:30 +0900

    MenusController::index()のテスト

---
+++ Test/Case/Controller/MenusController/IndexTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * MenusController::index()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * MenusController::index()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Menus\Test\Case\Controller\MenusController
+ */
+class MenusControllerIndexTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.menus.menu_frame_setting',
+		'plugin.menus.menu_frames_page',
+		'plugin.menus.menu_frames_room',
+		'plugin.menus.page4menu',
+		'plugin.menus.pages_language4menu',
+		'plugin.rooms.room4test',
+		'plugin.rooms.rooms_language4test',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'menus';
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	protected $_controller = 'menus';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//ログイン
+		TestAuthGeneral::login($this);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		//ログアウト
+		TestAuthGeneral::logout($this);
+
+		parent::tearDown();
+	}
+
+/**
+ * index()アクションのGetリクエストテスト
+ *
+ * @return void
+ */
+	public function testIndexGet() {
+		//テスト実行
+		$this->_testGetAction(array('action' => 'index', 'frame_id' => '6'), array('method' => 'assertNotEmpty'), null, 'view');
+
+		//チェック
+		debug($this->view);
+		$this->assertTextContains('<nav ng-controller="MenusController">', $this->view);
+	}
+
+}
