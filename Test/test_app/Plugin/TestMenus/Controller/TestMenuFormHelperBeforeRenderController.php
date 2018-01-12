453a5707ec86890f8816c6bf51f7fd669dc58c4d
Mitsuru Mutaguchi <akagane99@gmail.com>
2018-01-12 09:52:03 +0900

    MenuFormHelper/BeforeRenderTest 復活

---
+++ Test/test_app/Plugin/TestMenus/Controller/TestMenuFormHelperBeforeRenderController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * MenuFormHelperテスト用Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppController', 'Controller');
+
+/**
+ * MenuFormHelperテスト用Controller
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Menus\Test\test_app\Plugin\TestMenus\Controller
+ */
+class TestMenuFormHelperBeforeRenderController extends AppController {
+
+/**
+ * 使用ヘルパー
+ *
+ * @var array
+ */
+	public $helpers = array(
+		'Menus.MenuForm'
+	);
+
+/**
+ * index
+ *
+ * @return void
+ */
+	public function index() {
+		$this->autoRender = true;
+	}
+
+}
